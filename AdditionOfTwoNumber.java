42763b817a1487727fa944be67a886e5a196fb02
Abhishek kumar gond <abhishekkumargond592@gmail.com>
2022-12-11 21:43:59 +0530

    First Commit

---
+++ AdditionOfTwoNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AdditionOfTwoNumber {
+    public static void main(String[] args) {
+        Scanner Sc =new Scanner(System.in);
+        System.out.print("Enter Your First Number : = ");
+        int a = Sc.nextInt();
+        System.out.print("Enter Your Second Number : = ");
+        int b = Sc.nextInt();
+
+        int sum = a+b;
+        System.out.println("Total : "+sum);
+    }
+}
