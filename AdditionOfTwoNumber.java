d735df12cf96caac12b40ade807b9ac0dea59c3b
Abhishek kumar gond <abhishekkumargond592@gmail.com>
2022-12-11 21:46:30 +0530

    Second Commit

--- AdditionOfTwoNumber.java
+++ AdditionOfTwoNumber.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class AdditionOfTwoNumber {
     public static void main(String[] args) {
         Scanner Sc =new Scanner(System.in);
-        System.out.print("Enter Your First Number : = ");
+        System.out.print("Enter Your First Number : =  ");
         int a = Sc.nextInt();
-        System.out.print("Enter Your Second Number : = ");
+        System.out.print("Enter Your Second Number : =  ");
         int b = Sc.nextInt();
 
         int sum = a+b;
-        System.out.println("Total : "+sum);
+        System.out.println("Total : "+ sum);
     }
 }
